12e224bc057dc208accab9f7c965482041c6a1a3
Augusto Moita <a94moita@outlook.com>
2018-11-19 20:07:52 -0300

    Saco comentarios de la api v3 de github

--- src/Util/RepoService/GitHubService.php
+++ src/Util/RepoService/GitHubService.php
@@ -11,8 +11,6 @@ class GitHubService implements RepoServiceInterface
 {
 	const ENDPOINT = 'https://api.github.com/graphql';
 	const TOKEN = '11eb8db6bf7f7784fc4e0f53c5bc5f7e6342ead5';
-	// const ENDPOINT = 'https://api.github.com';
-	// const SEARCH_URL = '/search/repositories?q=';
 
 	public function search(string $q): array
 	{
@@ -41,31 +39,7 @@ class GitHubService implements RepoServiceInterface
 		$response = $graphql->query($query, $variables, self::TOKEN);
 
 	    return $this->toJson($response);
-
-    	/* $url = self::ENDPOINT.self::SEARCH_URL.$q;
-
-		$ch = \curl_init();
-		\curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		\curl_setopt($ch, CURLOPT_URL, $url);
-		\curl_setopt($ch, CURLOPT_USERAGENT, 'Ubykuo-App');
-		$response = \curl_exec($ch);
-		\curl_close($ch);
-
-		return $this->toJson2($response); */
 	}
-
-	/* private function toJson2($response): array
-	{
-		return array_map(function ($repo) {
-			return [
-				'id' => $repo->id,
-				'name' => $repo->name,
-				'full_name' => $repo->full_name,
-				'description' => $repo->description,
-				'url' => $repo->html_url
-			];
-		}, json_decode($response)->items);
-	} */
 
 	private function toJson($response): array
 	{
